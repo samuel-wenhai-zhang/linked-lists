f8fc60505aa8f5eb28f1079a929becc118659c56
Boltcrasher <Boltcrasher@gmail.com>
2023-11-01 13:32:06 -0500

    done?

--- Lab1/Sorted.java
+++ Lab1/Sorted.java
@@ -4,7 +4,25 @@ import java.io.File;
 
 public class Sorted {
     public static void main(String[] args) throws FileNotFoundException {
-        
-        
+        Scanner in = new Scanner(new File("sorted.dat"));
+        while (in.hasNextLine()) {
+            SortedLinkedList list = new SortedLinkedList();
+
+            String line = in.nextLine();
+            Scanner reader = new Scanner(line);            
+            while (reader.hasNextInt()) {
+                list.add(reader.nextInt());
+            }
+
+            String result = "";
+            for (int i = 0; i < 5; i++) {
+                result += list.removeFront() + " ";
+            }
+            for (int i = 0; i < 5; i++) {
+                result += list.removeBack() + " ";
+            }
+            System.out.println(result);
+        }
+        in.close();
     }    
 }
--- Lab1/SortedLinkedList.java
+++ Lab1/SortedLinkedList.java
@@ -7,43 +7,125 @@ public class SortedLinkedList {
     private int size;
 
     public SortedLinkedList() {
-        
+        head = null;
+        tail = null;
+        size = 0;
     }
 
     public void add(int n) {
-        
+        if (size == 0) {
+            Node node = new Node(n, null, null);     
+            head = node;
+            tail = node;
+        }
+        else if (n <= head.data) {
+            Node node = new Node(n, null, head);
+            head.prev = node;
+            head = node;
+        }
+        else if (n >= tail.data) {
+            Node node = new Node(n, tail, null);
+            tail.next = node;
+            tail = node;
+        }
+        else {
+            Node nextNode = head;
+            while (n > nextNode.data) {
+                nextNode = nextNode.next;
+            }
+            Node prevNode = nextNode.prev;
+            Node node = new Node(n, prevNode, nextNode);
+            prevNode.next = node;
+            nextNode.prev = node;
+        }
+        size++;
     }
 
     public void addAll(int[] ns) {
-        
+        for (int n : ns) {
+            add(n);
+        }        
     }
 
     public int removeFront() {
-        return -1;
+        if (size == 0) {
+            throw new NoSuchElementException();
+        }
+        Node oldNode = head;
+        if (size == 1) {
+            head = null;
+            tail = null;
+        }
+        else {
+            head = oldNode.next;
+            head.prev = null;
+        }
+        size--;
+        return oldNode.data;
     }
 
     public int peekFront() {
-        return -1;
+        if (size == 0) {
+            throw new NoSuchElementException();
+        }
+        return head.data;
     }
 
     public int removeBack() {
-        return -1;
+        if (size == 0) {
+            throw new NoSuchElementException();
+        }
+        Node oldNode = tail;
+        if (size == 1) {
+            head = null;
+            tail = null;
+        }
+        else {
+            tail = oldNode.prev;
+            tail.next = null;
+        }
+        size--;
+        return oldNode.data;
     }
 
     public int peekBack() {
-        return -1;
+        if (size == 0) {
+            throw new NoSuchElementException();
+        }
+        return tail.data;
     }
 
     public int size() {
-        return -1;
+        return size;
     }
 
+    @Override
     public String toString() {
-        return "";
+        if (size == 0) {
+            return "[]";
+        }
+        String result = "[";
+        result += head.data;
+
+        Node nextNode = head.next;
+        while (nextNode != null) {
+            result += ", " + nextNode.data;
+            nextNode = nextNode.next;
+        }
+        result += "]";
+        return result;
     }
 
     private static class Node {
-        
+        private int data;
+        private Node prev;
+        private Node next;
+
+        private Node(int data, Node prev, Node next) {
+            this.data = data;
+            this.prev = prev;
+            this.next = next;
+        }
     }
 
 }
